dd20e1c39bee5b6e71a2c02992ed31096704e1a7
hakohumi <mihumihu32@gmail.com>
2021-04-25 16:59:11 +0900

    add: average sensor data

--- Ambient_ESP_BME280_ds.ino
+++ Ambient_ESP_BME280_ds.ino
@@ -18,6 +18,7 @@
 
 // INA266用 追加
 // 参考 : https://asukiaaa.blogspot.com/2019/10/ina226arduino.html
+// https://github.com/asukiaaa/INA226_asukiaaa
 #include <INA226_asukiaaa.h>
 
 // もし、I2Cのポートを変更する場合は、wire2関数あたりで設定する必要あり
@@ -30,54 +31,27 @@
 #define INA226_1_ADDRESS 0x40  // ソーラー側INA226 スレーブアドレス
 #define INA226_2_ADDRESS 0x41  // バッテリー側INA226 スレーブアドレス
 
-// ambient送信間隔
-// 1時間
-#define SEND_DATA_AMBIENT
+// センサからデータを取得して、データをambientに送信する間隔 単位は[s]
+// 1時間 = 60分 * 60秒 = 3600秒
+#define SEND_DATA_AMBIENT_INTERVAL_SEC 3600
 
-温度、湿度、気圧
-    電流、電圧、電力
+// 平均化のために取得するデータの個数
+#define SENSING_DATA_COUNT 3
 
-    1秒間隔 3回
-
-    // シャント抵抗の値を引数にすると、キャリブレーションの値を出力する関数
-    // 2つのINA226でそれぞれシャント抵抗が違う場合は、それぞれの抵抗値で計算した値が必要
-    const uint16_t ina226calib = INA226_asukiaaa::calcCalibByResistorMilliOhm(2);  // Max 5120 milli ohm
+// シャント抵抗の値を引数にすると、キャリブレーションの値を出力する関数
+// 2つのINA226でそれぞれシャント抵抗が違う場合は、それぞれの抵抗値で計算した値が必要
+const uint16_t ina226calib = INA226_asukiaaa::calcCalibByResistorMilliOhm(2);  // Max 5120 milli ohm
 
 // 1つ目のINA226のスレーブアドレス
-INA226_asukiaaa voltCurrMeter_1(INA226_1_ADDRESS, ina226calib);  // 1つ目のINA226
-
-// 2つ目のセンサ
-// 〇〇〇の部分に、設定したスレーブアドレスと同じ定数（INA226_asukiaaa.hのdefineで設定されている）を指定する
-// ヘッダファイル INA226_asukiaaa.h の定義部分
-// これらから選ぶ
-/*
-#define INA226_ASUKIAAA_ADDR_A0_GND_A1_GND B1000000
-#define INA226_ASUKIAAA_ADDR_A0_VDD_A1_GND B1000001
-#define INA226_ASUKIAAA_ADDR_A0_SDA_A1_GND B1000010
-#define INA226_ASUKIAAA_ADDR_A0_SCL_A1_GND B1000011
-#define INA226_ASUKIAAA_ADDR_A0_GND_A1_VDD B1000100
-#define INA226_ASUKIAAA_ADDR_A0_VDD_A1_VDD B1000101
-#define INA226_ASUKIAAA_ADDR_A0_SDA_A1_VDD B1000110
-#define INA226_ASUKIAAA_ADDR_A0_SCL_A1_VDD B1000111
-#define INA226_ASUKIAAA_ADDR_A0_GND_A1_SDA B1001000
-#define INA226_ASUKIAAA_ADDR_A0_VDD_A1_SDA B1001001
-#define INA226_ASUKIAAA_ADDR_A0_SDA_A1_SDA B1001010
-#define INA226_ASUKIAAA_ADDR_A0_SCL_A1_SDA B1001011
-#define INA226_ASUKIAAA_ADDR_A0_GND_A1_SCL B1001100
-#define INA226_ASUKIAAA_ADDR_A0_VDD_A1_SCL B1001101
-#define INA226_ASUKIAAA_ADDR_A0_SDA_A1_SCL B1001110
-#define INA226_ASUKIAAA_ADDR_A0_SCL_A1_SCL B1001111
-*/
-
-INA226_asukiaaa voltCurrMeter_2(INA226_2_ADDRESS, ina226calib);  // 2つ目のINA226
+INA226_asukiaaa voltCurrMeter_1(INA226_1_ADDRESS, ina226calib);
+// 2つ目のINA226のスレーブアドレス
+INA226_asukiaaa voltCurrMeter_2(INA226_2_ADDRESS, ina226calib);
 
 // -----------------------------------------------------------------------
 
 extern "C" {
 #include "user_interface.h"
 }
-
-#define PERIOD 300
 
 BME280I2C bme280;  // Default : forced mode, standby time = 1000 ms
                    // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
@@ -98,7 +72,7 @@ void setup() {
     Serial.begin(SERIAL_BAUD);
     delay(10);
 
-    Serial.println("Start");
+    Serial.println("Wake up now");
 
     // BME280 setup
     while (!bme280.begin()) {
@@ -131,10 +105,6 @@ void setup() {
 
     // 消費電力センサの初期化
 
-    // 別ポートのI2Cポートを使用する場合
-    // Wire2.begin(I2C_SDA, I2C_SCL);
-    // voltCurrMeter.setWire(&Wire2);
-
     // センサ1つ目 がエラー
     if (voltCurrMeter_1.begin() != 0) {
         Serial.println("Failed to begin INA226_1");
@@ -147,95 +117,116 @@ void setup() {
     // ----------------------------------------
     // データ取得
     // ----------------------------------------
+    /*
+    温度、湿度、気圧
+    電流、電圧、電力
 
-    // 変数 初期化
-    // float temp(NAN), hum(NAN), pres(NAN);
-    float temp = NAN, hum = NAN, pres = NAN;
-    BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);  // 温度の単位設定
-    BME280::PresUnit presUnit(BME280::PresUnit_Pa);       // 気圧の単位設定
+    1秒間隔 3回 平均
+    */
+    float temp_total = 0.0f, hum_total = 0.0f, pres_total = 0.0f;  // 平均するようの変数
+    uint8_t BME280_read_count = 0;
 
-    // データ取得
-    bme280.read(pres, temp, hum, tempUnit, presUnit);
-    
-    Serial.print("temp: ");
-    Serial.print(temp);
-    Serial.print(", humid: ");
-    Serial.print(hum);
-    Serial.print(", pressure: ");
-    Serial.println(pres);
+    int16_t ma1_total, mv1_total;             // ina226_1 の平均値合計用の変数
+    int16_t ma2_total, mv2_total, mw2_total;  // ina226_2 の平均値合計用の変数
+    uint8_t INA226_1_read_count = 0;
+    uint8_t INA226_2_read_count = 0;
 
     // ----------------------------------------
+    // 3回データを1秒間隔で取得する
+    for (int c = 0; c < SENSING_DATA_COUNT; c++) {
+        // 変数 初期化
+        // float temp(NAN), hum(NAN), pres(NAN);
+        float temp = NAN, hum = NAN, pres = NAN;
 
-    // INA226 からデータを取得する
-    // センサからの値を格納する変数
-    // 多分、電流（mA）、電圧（mV）、電力（mW）
-    int16_t ma1, mv1, mw1;  // センサ1 用
-    int16_t ma2, mv2, mw2;  // センサ2 用
+        BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);  // 温度の単位設定
+        BME280::PresUnit presUnit(BME280::PresUnit_Pa);       // 気圧の単位設定
 
-    // 電圧を取得する
-    // 取得できたら、mvの変数に格納される
-    // 取得できなかったら、0が返ってくる（mvの変数の値は変わらず）
-    if (voltCurrMeter_1.readMV(&mv1) == 0) {
-        Serial.println(String(mv1) + "mV");
-    } else {
-        // 取得できなかった
-        Serial.println("Cannot read voltage.");
+        // データ取得
+        bme280.read(pres, temp, hum, tempUnit, presUnit);
+
+        Serial.print("temp: ");
+        Serial.print(temp);
+        Serial.print(", humid: ");
+        Serial.print(hum);
+        Serial.print(", pressure: ");
+        Serial.println(pres);
+
+        // 平均計算用
+        temp_total += temp;
+        hum_total += hum;
+        pres_total += pres;
+        BME280_read_count++;
+
+        // INA226 からデータを取得する
+        // センサからの値を格納する変数
+        // 多分、電流（mA）、電圧（mV）、電力（mW）
+        int16_t ma1, mv1;       // センサ1 用
+        int16_t ma2, mv2, mw2;  // センサ2 用
+
+        // 電圧・電流を取得する
+        // 取得できたら、mvの変数に格納される
+        // 取得できなかったら、0が返ってくる（mvの変数の値は変わらず）
+        if ((voltCurrMeter_1.readMV(&mv1) + voltCurrMeter_1.readMA(&ma1)) == 0) {
+            Serial.println(String(mv1) + "mV");
+            Serial.println(String(ma1) + "mA");
+            mv1_total += mv1;       // 平均計算用
+            ma1_total += ma1;       // 平均計算用
+            INA226_1_read_count++;  // 平均計算用
+        } else {
+            // 取得できなかった
+            Serial.println("Cannot read INA226_1.");
+        }
+
+        // 電圧・電流・電力を取得する
+        // 取得できたら、mvの変数に格納される
+        // 取得できなかったら、0が返ってくる（mvの変数の値は変わらず）
+        if ((voltCurrMeter_2.readMV(&mv2) + voltCurrMeter_2.readMA(&ma2) + voltCurrMeter_2.readMW(&mv2)) == 0) {
+            Serial.println(String(mv2) + "mV");
+            Serial.println(String(ma2) + "mA");
+            Serial.println(String(mv2) + "mW");
+            mv2_total += mv2;       // 平均計算用
+            ma2_total += ma2;       // 平均計算用
+            mw2_total += mw2;       // 平均計算用
+            INA226_2_read_count++;  // 平均計算用
+        } else {
+            // 取得できなかった
+            Serial.println("Cannot read watt.");
+        }
+
+        delay(1000);  // 平均用にセンサ値を取得する間隔
     }
-
-    // 上の電流版
-    if (voltCurrMeter_1.readMA(&ma1) == 0) {
-        Serial.println(String(ma1) + "mA");
-    } else {
-        // 取得できなかった
-        Serial.println("Cannot read current.");
-    }
-
-    // 電圧を取得する
-    // 取得できたら、mvの変数に格納される
-    // 取得できなかったら、0が返ってくる（mvの変数の値は変わらず）
-    if (voltCurrMeter_2.readMV(&mv2) == 0) {
-        Serial.println(String(mv2) + "mV");
-    } else {
-        // 取得できなかった
-        Serial.println("Cannot read voltage.");
-    }
-
-    // 上の電流版
-    if (voltCurrMeter_2.readMA(&ma2) == 0) {
-        Serial.println(String(ma2) + "mA");
-    } else {
-        // 取得できなかった
-        Serial.println("Cannot read current.");
-    }
-
-    // 上の電力版
-    if (voltCurrMeter_2.readMW(&mv2) == 0) {
-        Serial.println(String(mv2) + "mW");
-    } else {
-        // 取得できなかった
-        Serial.println("Cannot read watt.");
-    }
-
     // --------------------------------------------
+
+    float average_temp = temp_total / (float)BME280_read_count;
+    float average_hum  = hum_total / (float)BME280_read_count;
+    float average_pres = pres_total / (float)BME280_read_count;
+    float average_mv1  = (float)mv1_total / (float)INA226_1_read_count;
+    float average_ma1  = (float)ma1_total / (float)INA226_1_read_count;
+    float average_mv2  = (float)mv2_total / (float)INA226_2_read_count;
+    float average_ma2  = (float)ma2_total / (float)INA226_2_read_count;
+    float average_mw2  = (float)mw2_total / (float)INA226_2_read_count;
 
     ambient.begin(channelId, writeKey, &client);  // チャネルIDとライトキーを指定してAmbientの初期化
 
-    ambient.set(1, temp);  // 温度をデータ1にセット
-    ambient.set(2, hum);   // 湿度をデータ2にセット
-    ambient.set(3, pres);  // 気圧をデータ3にセット
+    ambient.set(1, average_temp);  // 温度をデータ1にセット
+    ambient.set(2, average_hum);   // 湿度をデータ2にセット
+    ambient.set(3, average_pres);  // 気圧をデータ3にセット
 
-    ambient.set(4, mv1);  // センサ1の電圧をデータ4にセット
-    ambient.set(5, ma1);  // センサ1の電力をデータ5にセット
-    ambient.set(6, ma2);  // センサ2の電流をデータ6にセット
-    ambient.set(7, mv2);  // センサ2の電圧をデータ7にセット
-    ambient.set(8, mw2);  // センサ2の電力をデータ8にセット
+    ambient.set(4, average_mv1);  // センサ1の電圧をデータ4にセット
+    ambient.set(5, average_ma1);  // センサ1の電流をデータ5にセット
+    ambient.set(6, average_mv2);  // センサ2の電圧をデータ6にセット
+    ambient.set(7, average_ma2);  // センサ2の電流をデータ7にセット
+    ambient.set(8, average_mw2);  // センサ2の電力をデータ8にセット
 
     ambient.send();  // データをAmbientに送信
 
     // --------------------------------------------
-
+    // プログラムの動作にかかった時間分
     t = millis() - t;
-    t = (t < PERIOD * 1000) ? (PERIOD * 1000 - t) : 1;
-    ESP.deepSleep(t * 1000, RF_DEFAULT);
-    delay(1000);
+    t = (t < SEND_DATA_AMBIENT_INTERVAL_SEC * 1000) ? (SEND_DATA_AMBIENT_INTERVAL_SEC * 1000 - t) : 1;
+
+    ESP.deepSleep(t * 1000, RF_DEFAULT);  // DeepSleepで復帰する時間[us]
+    delay(1000);                          // DeepSleepに入るための準備時間
 }
+
+void loop() {}
